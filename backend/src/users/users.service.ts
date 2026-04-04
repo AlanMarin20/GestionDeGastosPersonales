380a76eeeb173411985c1c35e794e2f0c7ff18a5
JoacoContreras <cjoaquin835@gmail.com>
2026-04-04 14:10:12 -0300

    feat(users): implementar metodo remove y completar CRUD de usuarios

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -35,15 +35,16 @@ export class UsersService {
     });
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  update(id: string, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    await this.userRepository.delete(id);
+    return { message: 'Usuario eliminado correctamente' };
   }
 }
