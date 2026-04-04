a2a3b1d6a8c091ac8ad6ad475bf341c3b8278fa7
JoacoContreras <cjoaquin835@gmail.com>
2026-04-04 13:46:59 -0300

    feat(users): agregar validacion con DTO y encriptacion de passwords con bcrypt

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    // 1. Extraemos la contraseña y el resto de los datos del DTO
+    const { password, ...userData } = createUserDto;
+
+    // 2. Encriptamos la contraseña (10 es el costo del algoritmo, es un buen estándar)
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    // 3. TypeORM crea la instancia fusionando los datos y el hash
+    const newUser = this.userRepository.create({ ...userData, passwordHash });
+
+    // 4. Guarda el usuario en la base de datos y retorna el resultado
+    return await this.userRepository.save(newUser);
   }
 
   findAll() {
