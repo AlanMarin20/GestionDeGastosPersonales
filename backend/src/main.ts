77564201c558a2dd898952985825f60fb3c54e91
Alan <marinalan396@gmail.com>
2026-06-21 18:44:18 -0300

    backend y frontend levantado, fallaba la conexion entre back y front

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     }),
   );
 
-  app.enableCors();
+  app.enableCors({ origin: '*' });
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port, '0.0.0.0');
